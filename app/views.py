2edd87078df8a542e2825348dbee787440165ccf
Reuben-Kipkemboi <reuben.kipkemboi@student.moringaschool.com>
2022-06-18 08:04:26 +0300

    ft:navbar - includes navbar

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .models import * 
+from django.contrib import messages
 
 # Application views.
 def home(request):
@@ -6,7 +8,24 @@ def home(request):
 
 
 def register(request):
+    if request.method =='POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2= request.POST['password2']
+        
+        if password1 != password2:
+            messages.error(request,"confirm your passwords")
+            return redirect('/register')
+        
+        new_user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password1)
+        
+        new_user.save()
+        return render(request,'login.html')
     return render(request, 'register.html')
+
 
 def user_login(request):
     return render(request, 'login.html')
