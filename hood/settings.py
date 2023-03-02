aa50ef65b1e446b8b2142b190bb2606402b036f9
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2023-03-02 09:23:43 +0300

    ft:set-up - set up project files

--- hood/settings.py
+++ hood/settings.py
@@ -110,15 +110,15 @@ WSGI_APPLICATION = 'hood.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbneighbour',
-        'USER': 'moringa',
-        'PASSWORD': 'Access',
-        'HOST': ''
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dbneighbour',
+#         'USER': 'moringa',
+#         'PASSWORD': 'Access',
+#         'HOST': ''
+#     }
+# }
 
 
 # Password validation
